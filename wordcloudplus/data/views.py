559ffef554b9f787b6814942c18c14c57115c1e1
CroyDaniel <dsc986@tamu.edu>
2017-03-10 02:48:37 -0600

    Added wordcloud generation to the landing page. Color is random. Position is random. Words are generated from scraped data.

--- wordcloudplus/data/views.py
+++ wordcloudplus/data/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 import mining
+import simplejson as json
 
 @csrf_exempt
 def index(request):
 	site_content_keys = []
 	site_content_values = []
+	json_string = '['
 	
 	if request.method == 'POST':
 		#print request.POST		#for testing / error handling
@@ -14,6 +16,8 @@ def index(request):
 		for i in site_content:
 			site_content_keys.append(i[0])
 			site_content_values.append(i[1])
+			json_string += (('' + i[0] + ', ') * i[1])
+		json_string = json_string[:-2] + ']'
 	else:
 		site_content = ''
 		
@@ -21,7 +25,8 @@ def index(request):
 	
 	c = {
 			'mined_data': site_content,
-			'zipped_counter': zipped_counter
+			'zipped_counter': zipped_counter,
+			'json_string': json_string
 		}
 	
 	return render(request, 'data/index.html', c)
