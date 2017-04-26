c86e6f6330a4cb4865713bf7b77bd481ac24ff31
m00sedog00 <m00sedog00@github.tamu.edu>
2017-04-26 01:07:05 -0500

    Massive dynamic structure merge. TODO: finish indexhtml/d3samplejs

--- wordcloudplus/data/mining.py
+++ wordcloudplus/data/mining.py
@@ -8,7 +8,7 @@ import string
 from nltk.stem import WordNetLemmatizer
 import json
 from django.core.serializers.json import DjangoJSONEncoder
-
+import copy
 #import nltk (for after CDR is complete to not mess with workflow)
 
 #init stopwords list
@@ -34,25 +34,55 @@ def scrape(site_address):
 # that are used on that webpage
 def get_data_string(site_address):
 	return scrape(site_address)
-	
+
+def calc_frequency(areaNum, word_counts, curr_timeslot_word_counts):
+	timeslot_area_percentage = {}
+
+	for i in word_counts:
+		both_contain_word = False
+		for j in curr_timeslot_word_counts:
+			#on same word (key) calculate actual percent
+			if i == j:
+				contain_word = True
+				timeslot_area_percentage[i] = (word_counts[i]/(curr_timeslot_word_counts[j] + 0.00)) * 100
+				timeslot_area_percentage[i] = round(timeslot_area_percentage[i], 2)
+
+	return timeslot_area_percentage
+	# if areaNum == 1:
+	# 	return {"area1" : timeslot_area_percentage} # TODO: convert to django friendly using below??
+	# elif areaNum == 2:
+	# 	return {"area2" : timeslot_area_percentage}
+	# elif areaNum == 3:
+	# 	return {"area3" : timeslot_area_percentage}
+
+	#convert site percents to django friendly JSON
+	# site1_percentage_json = json.dumps(dict(site1_percentage), cls=DjangoJSONEncoder)
+	# site2_percentage_json = json.dumps(dict(site2_percentage), cls=DjangoJSONEncoder)
+
 # takes a url as a string and returns a SET of TOUPLES of all of the words
 # that are used on that webpage in order of frequency
-def get_data_set(addresses = [], *args):
-	first_iteration = True
-	data_list_postprocessing = []
-	site1_percentage = {}
-	site2_percentage = {}
+def get_data_set(area1addr = [], area2addr = [], area3addr = [], years = [], *args):
+	index_counter = 0
+	#FOR SIZE: Total word counts per year { 1995 : {blah : 50, hey : 23, etc} }
+	timeslot_word_counts = {}
+	#FOR FREQUENCY: percentages of each word per source in each timeslot
+	#i.e. { 1995 : {"area1" : {'word' : 33.33%, 'lol' : 69%, etc}, "area2" : {'word' : 66.67%, 'lol' : 31%, etc}} }
+	timeslot_word_frequency = {}
 
-	#iterates through each site in ddresses[]
-	for site in addresses:
+	#dictionary sorting stored objects by year
+	# { 1995 : {"area1" : Data, "area2" : Data}, 2013 : {"area3" : Data} }
+	# WHERE Data = ["wrod", "hi", "adsf"]
+	post_processed_storage = {}
+
+	while index_counter < len(area1addr):
+		site = area1addr[index_counter]
 		current_site_list = []
-		#if nonempty entry
+		#if nonemtpy entry
 		if site:
-		#	print 'site addresses[] = ' + site
 			data_string = scrape(site)
 			data_list = data_string.split(' ')
-			
-			# the following removes new lines and punctionation from the data in the set 
+
+			#preprocessing the scraped dataset
 			temp = ""
 			for i in data_list:
 				#remove all whitespace
@@ -68,57 +98,154 @@ def get_data_set(addresses = [], *args):
 				#if nonempty str
 				if temp and (len(temp) > 2) and (not temp.isdigit()):#if nonempty str
 					current_site_list.append((lemmatizer.lemmatize(temp.decode('utf-8'))))
-					data_list_postprocessing.append((lemmatizer.lemmatize(temp.decode('utf-8'))))
+					#data_list_postprocessing.append((lemmatizer.lemmatize(temp.decode('utf-8'))))
 
-		#track word freq from first site
-		#TODO: automate for n>2 sources
-		if first_iteration:
-			site1_count = collections.Counter(data_list_postprocessing)
-			first_iteration = False
-		else:
-			site2_count = collections.Counter(current_site_list)
+			area1Data = {"area1" : current_site_list}
+
+			#if year exists in storage, extend onto processed data to list at that year
+			if years[index_counter] in post_processed_storage:
+				#if data from area exists in storage, extend that data
+				if "area1" in post_processed_storage[years[index_counter]]:
+					post_processed_storage[years[index_counter]]["area1"].extend(area1Data.values()[0])
+				#else create new data entry
+				else:
+					post_processed_storage[years[index_counter]]["area1"] = (area1Data.values()[0])
+			#else, create new year in storage, add list w/ 1 value (processed data)
+			else:
+				post_processed_storage[years[index_counter]] = area1Data
+
+		index_counter+= 1
+
+	while index_counter < (len(area1addr) + len(area2addr)):
+		site = area2addr[index_counter - len(area1addr)]
+		current_site_list = []
+		#if nonemtpy entry
+		if site:
+			data_string = scrape(site)
+			data_list = data_string.split(' ')
+
+			#preprocessing the scraped dataset
+			temp = ""
+			for i in data_list:
+				#remove all whitespace
+				temp = ''.join(i.split())	#remove all whitespace
+				#enforces utf-8
+				temp = temp.encode('utf-8')
+				#removes punctuation
+				temp = temp.translate(None, string.punctuation)
+				temp = temp.lower()	#lowercase
+				#remove stop words
+				temp = ''.join([word for word in temp.split() if word not in stopwords])
+		
+				#if nonempty str
+				if temp and (len(temp) > 2) and (not temp.isdigit()):#if nonempty str
+					current_site_list.append((lemmatizer.lemmatize(temp.decode('utf-8'))))
+					#data_list_postprocessing.append((lemmatizer.lemmatize(temp.decode('utf-8'))))
+
+			area2Data = {"area2" : current_site_list}
+
+			#if year exists in storage, extend onto processed data to list at that year
+			if years[index_counter] in post_processed_storage:
+				#if data from area exists in storage, extend that data
+				if "area2" in post_processed_storage[years[index_counter]]:
+					post_processed_storage[years[index_counter]]["area2"].extend(area2Data.values()[0])
+				#else create new data entry
+				else:
+					post_processed_storage[years[index_counter]]["area2"] = (area2Data.values()[0])
+			#else, create new year in storage, add list w/ 1 value (processed data)
+			else:
+				post_processed_storage[years[index_counter]] = area2Data
+
+		index_counter+= 1
+
+	while index_counter < (len(area1addr) + len(area2addr) + len(area3addr)):
+		site = area3addr[index_counter - (len(area1addr) + len(area2addr))]
+		current_site_list = []
+		#if nonemtpy entry
+		if site:
+			data_string = scrape(site)
+			data_list = data_string.split(' ')
+
+			#preprocessing the scraped dataset
+			temp = ""
+			for i in data_list:
+				#remove all whitespace
+				temp = ''.join(i.split())	#remove all whitespace
+				#enforces utf-8
+				temp = temp.encode('utf-8')
+				#removes punctuation
+				temp = temp.translate(None, string.punctuation)
+				temp = temp.lower()	#lowercase
+				#remove stop words
+				temp = ''.join([word for word in temp.split() if word not in stopwords])
+		
+				#if nonempty str
+				if temp and (len(temp) > 2) and (not temp.isdigit()):#if nonempty str
+					current_site_list.append((lemmatizer.lemmatize(temp.decode('utf-8'))))
+					#data_list_postprocessing.append((lemmatizer.lemmatize(temp.decode('utf-8'))))
+
+			area3Data = {"area3" : current_site_list}
+
+			#if year exists in storage, extend processed data onto list at that year
+			if years[index_counter] in post_processed_storage:
+				#if data from area exists in storage, extend that data
+				if "area3" in post_processed_storage[years[index_counter]]:
+					post_processed_storage[years[index_counter]]["area3"].extend(area3Data.values()[0])
+				#else create new data entry
+				else:
+					post_processed_storage[years[index_counter]]["area3"] = (area3Data.values()[0])
+			#else, create new year in storage, add list w/ 1 value (processed data)
+			else:
+				post_processed_storage[years[index_counter]] = area3Data
+
+		index_counter+= 1
 	
-	#data_set_postprocessing = set(data_list_postprocessing)	#converts list into set which
-												#removes duplicates
-	counter = collections.Counter(data_list_postprocessing)
-	
-	#assert how much % of the frequency the sites contribute
-	for i in site1_count:
-		both_contain_word = False
-		for j in site2_count:
-			#on same word (key) calculate actual percent
-			if i == j:
-				both_contain_word = True
-				sum = site1_count[i] + site2_count[j]
-				site1_percentage[i] = (site1_count[i]/(sum + 0.00)) * 100
-				site1_percentage[i] = round(site1_percentage[i], 2)
-				site2_percentage[j] = (100.00 - site1_percentage[i])
-		#in absence of same word, denote 100%
-		if not both_contain_word:
-			site1_percentage[i] = 100.00
-			site2_percentage[i] = 0
+	#TODO: in testing check word_counts, make sure it doesn't get weird
+	#FOR FONT SIZE: Accesses each timeslot and creates total count
+	for timeslot in post_processed_storage:
+		complete_word_list = []
+		dict_of_data = post_processed_storage[timeslot]
+		first_index = True
 
-	#horrible innefficient check for site2 words not in site1
-	#guys it's 5am and i have 2 exams today i havent studied for lol
-	for j in site2_count:
-		both_contain_word = False
-		for i in site1_count:
-			if j == i:
-				both_contain_word = True
-		#in absence of same word, denote 100%
-		if not both_contain_word:
-			site2_percentage[j] = 100.00
-			site1_percentage[j] = 0
+		#print dict_of_data
+		for areatype, word_list in dict_of_data.iteritems():
+			word_list_var = copy.deepcopy(word_list)
+			if first_index:
+				complete_word_list = word_list_var
+				first_index = False
+			else:
+				complete_word_list.extend(word_list_var)
 
-	#convert site percents to django friendly JSON
-	site1_percentage_json = json.dumps(dict(site1_percentage), cls=DjangoJSONEncoder)
-	site2_percentage_json = json.dumps(dict(site2_percentage), cls=DjangoJSONEncoder)
+		#comp_word_list_dict = dict(collections.Counter(complete_word_list))
+		# Creates list of tuples of words,freq at that timeslot, sorted descending
+		timeslot_word_counts[timeslot] = (collections.Counter(complete_word_list)).most_common()
+		# Converts tuples into dictionary format to not be shit
+		timeslot_word_counts[timeslot] = dict(timeslot_word_counts[timeslot])
+
+	#FOR FREQUENCY: 
+	for timeslot in post_processed_storage:
+		dict_of_data = post_processed_storage[timeslot]
+		timeslot_word_frequency[timeslot] = {}
+
+		for areatype, word_list in dict_of_data.iteritems():
+			if (areatype == "area1"):
+				word_counts = collections.Counter(word_list)
+				freq = (calc_frequency(1, word_counts, timeslot_word_counts[timeslot]))
+				timeslot_word_frequency[timeslot]["area1"] = freq
+			elif (areatype == "area2"):
+				word_counts = collections.Counter(word_list)
+				freq = (calc_frequency(2, word_counts, timeslot_word_counts[timeslot]))
+				timeslot_word_frequency[timeslot]["area2"] = freq
+			elif (areatype == "area3"):
+				word_counts = collections.Counter(word_list)
+				freq = (calc_frequency(3, word_counts, timeslot_word_counts[timeslot]))
+				timeslot_word_frequency[timeslot]["area3"] = freq
 
 	#wordcloud dictionary of objects
-	w = {
-			'site_content' : counter.most_common(),
-			'site1_percentage_json' : site1_percentage_json,
-			'site2_percentage_json' : site2_percentage_json
+		w = {
+			'post_processed_storage' : post_processed_storage,
+			'timeslot_word_counts' : timeslot_word_counts,
+			'timeslot_word_frequency' : timeslot_word_frequency
 		}
 
 	return w
--- wordcloudplus/data/views.py
+++ wordcloudplus/data/views.py
@@ -5,22 +5,9 @@ import simplejson as json
 from lxml import html, etree
 from lxml.html.clean import clean_html
 from data.models import Docs
-import textract
 
 @csrf_exempt
 def index(request):
-	site_content_keys1 = []
-	site_content_values1 = []
-	site_content_json1 = '['
-	wordcloud_object_dict1 = {}
-	site_content_keys2 = []
-	site_content_values2 = []
-	site_content_json2 = '['
-	wordcloud_object_dict2 = {}
-	site_content_keys3 = []
-	site_content_values3 = []
-	site_content_json3 = '['
-	wordcloud_object_dict3 = {}
 	"""
 	if request.method == 'POST':
 		#print request.POST		#for testing / error handling
@@ -60,88 +47,32 @@ def index(request):
 			#site_content, site1_percentage, site2_percentage
 		
 		years = request.POST.getlist('years')
-		wordcloud_object_dict1 = mining.get_data_set(request.POST.getlist('firstAddresses'))
-		wordcloud_object_dict2 = mining.get_data_set(request.POST.getlist('secondAddresses'))
-		wordcloud_object_dict3 = mining.get_data_set(request.POST.getlist('thirdAddresses'))
+		area1addr = request.POST.getlist('firstAddresses')
+		area2addr = request.POST.getlist('secondAddresses')
+		area3addr = request.POST.getlist('thirdAddresses')
 
-		site_content1 = wordcloud_object_dict1['site_content']
-		site1_percentage_json1 = wordcloud_object_dict1['site1_percentage_json']
-		site2_percentage_json1 = wordcloud_object_dict1['site2_percentage_json']
+		wordcloud_object = mining.get_data_set(area1addr, area2addr, area3addr, years)
+		post_processed_storage = wordcloud_object['post_processed_storage']
+		timeslot_word_counts = wordcloud_object['timeslot_word_counts']
+		timeslot_word_frequency = wordcloud_object['timeslot_word_frequency']
+		post_processed_storage = json.dumps(post_processed_storage, ensure_ascii=False)
+		timeslot_word_counts = json.dumps(timeslot_word_counts, ensure_ascii=False)
+		timeslot_word_frequency = json.dumps(timeslot_word_frequency, ensure_ascii=False)
 
-		site_content2 = wordcloud_object_dict2['site_content']
-		site1_percentage_json2 = wordcloud_object_dict2['site1_percentage_json']
-		site2_percentage_json2 = wordcloud_object_dict2['site2_percentage_json']
-
-		site_content3 = wordcloud_object_dict3['site_content']
-		site1_percentage_json3 = wordcloud_object_dict3['site1_percentage_json']
-		site2_percentage_json3 = wordcloud_object_dict3['site2_percentage_json']
-
-		for i in site_content1:
-			site_content_keys1.append(i[0])
-			site_content_values1.append(i[1])
-			site_content_json1 += (('' + i[0] + ', ') * i[1])
-		site_content_json1 = site_content_json1[:-2] + ']'
-
-		for i in site_content2:
-			site_content_keys2.append(i[0])
-			site_content_values2.append(i[1])
-			site_content_json2 += (('' + i[0] + ', ') * i[1])
-		site_content_json2 = site_content_json2[:-2] + ']'
-
-		for i in site_content3:
-			site_content_keys3.append(i[0])
-			site_content_values3.append(i[1])
-			site_content_json3 += (('' + i[0] + ', ') * i[1])
-		site_content_json3 = site_content_json3[:-2] + ']'
+		# print timeslot_word_counts
+		# print timeslot_word_frequency
 
 	else:
 		years = ''
-		site_content1 = ''
-		site1_percentage_json1 = ''
-		site2_percentage_json1 = ''
+		post_processed_storage = ''
+		timeslot_word_counts = ''
+		timeslot_word_frequency = ''
 
-		site_content2 = ''
-		site1_percentage_json2 = ''
-		site2_percentage_json2 = ''
-
-		site_content3 = ''
-		site1_percentage_json3 = ''
-		site2_percentage_json3 = ''
-		"""
-		#Stores file in database
-		#then reads file
-		#then deletes files
-		if request.POST['file_upload']:
-			file=request.FILES['doc']
-		# 	print file.name
-			
-			instance = Docs(	file=request.FILES['doc'],
-								title = 'temp',
-							)
-			instance.save()
-
-			file_name = file.name
-			file_name = file_name.replace(" ", "_")
-			path = 'uploads/' + file_name
-			
-			text = textract.process(path)
-			print text
-			instance.delete()
-			
-		else:
-			wordcloud_object_dict = mining.get_data_set(request.POST.getlist('addresses'))
-		"""
 	c = {
-			'site_content_json1':site_content_json1,
-			'site1_percentage_json1':site1_percentage_json1,
-			'site2_percentage_json1':site2_percentage_json1,
-			'site_content_json2':site_content_json2,
-			'site1_percentage_json2':site1_percentage_json2,
-			'site2_percentage_json2':site2_percentage_json2,
-			'site_content_json3':site_content_json3,
-			'site1_percentage_json3':site1_percentage_json3,
-			'site2_percentage_json3':site2_percentage_json3,			
-			'years':years
-		}
+		'post_processed_storage' : post_processed_storage,
+		'timeslot_word_counts' : timeslot_word_counts,
+		'timeslot_word_frequency' : timeslot_word_frequency,
+		'years' : years
+	}
 
 	return render(request, 'data/index.html', c)
